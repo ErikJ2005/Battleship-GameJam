ec148a751b49b41f693119153ff9c7fb75b1c995
ErikJ2005 <erik.w.jenkins@gmail.com>
2025-03-07 22:21:11 +0100

    Rettet skrivefeil

--- network.py
+++ network.py
@@ -3,7 +3,7 @@ import socket
 class Network():
     def __init__(self, ip):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = ip # Netverks ip
+        self.host = ip # Nettverks ip
 
         self.port = 5555
         self.addr = (self.host, self.port)
